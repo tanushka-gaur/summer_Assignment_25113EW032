7f9a908edf707c5bd7c8552f4aba15a7ef5e7064
tanushka-gaur <288720363+tanushka-gaur@users.noreply.github.com>
2026-06-04 23:39:15 +0530

    DAY-4 Uploaded

---
+++ DAY-4/Q15.cpp
@@ -0,0 +1,30 @@
+// Write a program to Check Armstrong number.
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int inputnum, original, currentDigit, Sum = 0;
+
+    cout << "Enter a number: ";
+    cin >> inputnum;
+
+    original = inputnum;
+
+    while (inputnum > 0)
+    {
+        currentDigit = inputnum % 10;
+
+        Sum = Sum + (currentDigit * currentDigit * currentDigit);
+
+        inputnum = inputnum / 10;
+    }
+
+    if (Sum == original)
+        cout << "Gicen number is Armstrong Number";
+    else
+        cout << "Given number is not an Armstrong Number";
+
+    return 0;
+}
