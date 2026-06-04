ec235b897fba9da927415c947b0ce0237db5a746
Tanushka Gaur <288720363+tanushka-gaur@users.noreply.github.com>
2026-06-04 16:03:07 +0530

    Update in prime number program

--- DAY-3/Q9.cpp
+++ DAY-3/Q9.cpp
@@ -1,34 +1,38 @@
-// Program to check whether the number is prime
+// Program to check whether a number is prime
 
 #include <iostream>
 using namespace std;
 
-int main() {
-    int n;
-    bool isPrime = true;
+int main() 
+{
+    int num,i,count= 0;
 
-    // Take Input number from the user
+    // Take input number from the user
     cout << "Enter a number: ";
-    cin >> n;
+    cin >> num;
 
     // Numbers less than or equal to 1 are not prime
-    if (n <= 1) {
-        isPrime = false;
-    } else {
-        // Check divisibility from 2 to n/2
-        for (int i = 2; i <= n / 2; i++) {
-            if (n % i == 0) {
-                isPrime = false;
-                break;
+    if (num <= 1) 
+    {
+        cout << num << " is not a Prime Number";
+    } 
+    else 
+    {
+        // Check divisibility of number
+        for (int i = 2; i < num; i++) 
+        {
+            if (num % i == 0) 
+            {
+                count++;
             }
         }
     }
 
-    // Display the result
-    if (isPrime)
-        cout << n << " is a Prime Number";
+    // If there is no other divisor except 1 and the number itself then given number is prime
+    if (count==0)
+        cout << num << " is a Prime Number";
     else
-        cout << n << " is not a Prime Number";
+        cout << num << " is not a Prime Number";
 
     return 0;
 }
