1d3cdffcb3a64ea0d8d6a15d889301125ae6d15d
tanushka-gaur <288720363+tanushka-gaur@users.noreply.github.com>
2026-06-08 23:29:59 +0530

    DAY-7 Uploaded

---
+++ DAY-7/Q27.cpp
@@ -0,0 +1,32 @@
+// Program to calculate Recursive Fibonacci
+
+#include <iostream>
+using namespace std;
+
+// Function to find Fibonacci term
+int fibonacci(int num)
+{
+    // First two terms
+    if (num == 0)
+        return 0;
+
+    if (num == 1)
+        return 1;
+
+    // Add previous two terms
+    return fibonacci(num - 1) + fibonacci(num - 2);
+}
+
+int main()
+{
+    int num;
+
+    // Take input from user
+    cout << "Enter term number: ";
+    cin >> num;
+
+    // Display result 
+    cout << "Fibonacci term = " << fibonacci(n);
+
+    return 0;
+}
