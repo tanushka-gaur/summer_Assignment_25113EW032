da967c1551ccea11868e163ef9b3ffa7f41ff2df
tanushka-gaur <288720363+tanushka-gaur@users.noreply.github.com>
2026-06-09 21:30:19 +0530

    DAY-8 Uploaded

--- DAY-8/Q29.cpp
+++ DAY-8/Q29.cpp
@@ -1,1 +1,38 @@
 // Program to Print half pyramid pattern
+
+/* 
+
+*
+* *
+* * *
+* * * *
+* * * * *
+
+*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int rows, i, j ;
+
+    // Take number of rows from user
+    cout << "Enter number of rows = ";
+    cin >> rows;
+
+    // Loop for rows
+    for (i = 1; i <= rows; i++)
+    {
+        // Print stars in each row
+        for (j = 1; j <= i; j++)
+        {
+            cout << "* ";
+        }
+
+        // Move to next line
+        cout << endl;
+    }
+
+    return 0;
+}
