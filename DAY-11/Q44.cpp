fab03e5fb1becc28e98dfb156b83e8064a9417aa
tanushka-gaur <288720363+tanushka-gaur@users.noreply.github.com>
2026-06-11 22:35:00 +0530

    DAY-11 Uploaded

---
+++ DAY-11/Q44.cpp
@@ -0,0 +1,30 @@
+// Function to find the factorial of a number
+
+
+#include <iostream>
+using namespace std;
+
+// Function to calculate factorial
+int factorial(int n)
+{
+    int fact = 1;
+
+    for (int i = 1; i <= n; i++)
+    {
+        fact = fact * i;
+    }
+
+    return fact;
+}
+
+int main()
+{
+    int num;
+
+    cout << "Enter a number: ";
+    cin >> num;
+
+    cout << "Factorial = " << factorial(num);
+
+    return 0;
+}
