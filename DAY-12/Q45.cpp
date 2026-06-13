3005b0c5fef663c88996105860c020e2e693886c
tanushka-gaur <288720363+tanushka-gaur@users.noreply.github.com>
2026-06-13 22:08:27 +0530

    DAY-12 Uploaded

---
+++ DAY-12/Q45.cpp
@@ -0,0 +1,51 @@
+// Program to check whether a number is Palindrome or not by function
+
+#include <iostream>
+using namespace std;
+
+// Function to check palindrome
+bool isPalindrome(int num)
+{
+    // Negative numbers are not palindrome
+    if (num < 0)
+    {
+        return false;
+    }
+
+    int rev = 0, original;
+
+    // Store original number for comparison
+    original = n;
+
+    // Reverse the number
+    while (num > 0)
+    {
+        int digit = num % 10;
+        rev = rev * 10 + digit;
+        num /= 10;
+    }
+
+    // Palindrome condition
+    return rev == original ;
+}
+
+int main()
+{
+    int num;
+
+    // Take input from user
+    cout << "Enter the number: ";
+    cin >> num;
+
+    // Display result
+    if (isPalindrome(num))
+    {
+        cout << "Palindrome Number\n";
+    }
+    else
+    {
+        cout << "Not a Palindrome Number\n";
+    }
+
+    return 0;
+}
