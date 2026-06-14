2a7cd2378c4752a74a619f5609022c5f898fb70a
tanushka-gaur <288720363+tanushka-gaur@users.noreply.github.com>
2026-06-14 18:37:14 +0530

    DAY-1 Uploaded

--- DAY-1/Q4.cpp
+++ DAY-1/Q4.cpp
@@ -10,12 +10,19 @@ int main() {
     cout << "Enter a number: ";
     cin >> num;
 
-    // Count digits by repeatedly dividing the number by 10
-    while (num != 0)
-    {
-        num = num / 10;  // Remove the last digit
-        count++;         // Increase digit count
-    }
+
+    if (num == 0) count = 1;
+
+    else
+    {   
+        // Count digits by repeatedly dividing the number by 10
+        while (num != 0)
+        {
+            num = num / 10;  // Remove the last digit
+            count++;         // Increase digit count
+        }
+        
+    }    
 
     // Display the total number of digits
     cout << "Number of digits = " << count;
