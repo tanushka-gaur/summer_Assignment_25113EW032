5045bf1e2f9aba4bf565da24c708f0c95386f18a
tanushka-gaur <288720363+tanushka-gaur@users.noreply.github.com>
2026-06-18 13:28:34 +0530

    DAY-14 Uploaded

---
+++ DAY-14/Q54.cpp
@@ -0,0 +1,36 @@
+// Program to Find Frequency of an Element in an Array
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n, key, count = 0;
+
+    cout << "Enter Array Length = ";
+    cin >> n;
+
+    int array[n];
+
+    // Input array elements
+    for (int i = 0; i < n; i++)
+    {
+        cout << "Enter array element " << (i + 1) << " = ";
+        cin >> array[i];
+    }
+
+    // Enter element whose frequency is to be found
+    cout << "Enter element to find frequency = ";
+    cin >> key;
+
+    // Count frequency
+    for (int i = 0; i < n; i++)
+    {
+        if (array[i] == key)
+            count++;
+    }
+
+    cout << "Frequency of " << key << " = " << count;
+
+    return 0;
+}
