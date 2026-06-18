9e6d6996819081f7059de3bf3477a36dcebbaa50
tanushka-gaur <288720363+tanushka-gaur@users.noreply.github.com>
2026-06-18 16:56:13 +0530

    DAY-17 Uploaded

---
+++ DAY-17/Q68.cpp
@@ -0,0 +1,50 @@
+// Program to Find Intersection of Two Arrays
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n1, n2;
+
+    cout << "Enter First Array Length = ";
+    cin >> n1;
+
+    int arr1[n1];
+
+    // Input first array
+    for (int i = 0; i < n1; i++)
+    {
+        cout << "Enter element " << i + 1 << " = ";
+        cin >> arr1[i];
+    }
+
+    cout << "Enter Second Array Length = ";
+    cin >> n2;
+
+    int arr2[n2];
+
+    // Input second array
+    for (int i = 0; i < n2; i++)
+    {
+        cout << "Enter element " << i + 1 << " = ";
+        cin >> arr2[i];
+    }
+
+    cout << "\nIntersection of Arrays = ";
+
+    // Find common elements
+    for (int i = 0; i < n1; i++)
+    {
+        for (int j = 0; j < n2; j++)
+        {
+            if (arr1[i] == arr2[j])
+            {
+                cout << arr1[i] << " ";
+                break;
+            }
+        }
+    }
+
+    return 0;
+}
