6745c290f9906e4de8c448da4570decc0e0be031
tanushka-gaur <288720363+tanushka-gaur@users.noreply.github.com>
2026-07-08 13:01:47 +0530

    Refactor Armstrong number range input and logic
    Refactor Armstrong number program to use lower and upper limits for range input. Added input validation for lower limit and adjusted the logic for calculating Armstrong numbers.

--- DAY-4/Q16.cpp
+++ DAY-4/Q16.cpp
@@ -1,40 +1,56 @@
 // Program to Print Armstrong numbers in a range
 
 #include <iostream>
+#include <cmath>
 using namespace std;
 
 int main()
 {
-    int start, end, num, currentdigit, sum, temp;
+    int lower, upper;
 
-    // Take input from user 
-    cout << "Enter starting number: ";
-    cin >> start;
+    cout << "Enter Lower Limit: ";
+    cin >> lower;
 
-    cout << "Enter ending number: ";
-    cin >> end;
-
-    cout << "Armstrong numbers in the range are: ";
-
-    for (num = start; num <= end; num++)
+    while (lower < 0)
     {
-        sum = 0;
+        cout << "Lower Limit can't be negative\nEnter again => ";
+        cin >> lower;
+    }
+
+    cout << "Enter Upper Limit: ";
+    cin >> upper;
+
+    cout << "Armstrong Number from " << lower << " to " << upper << ": ";
+
+    for (int num = lower; num <= upper; num++)
+    {
+        int temp = num;
+        int sum = 0;
+        int num_digits = 0;
+        int digit;
+
+        /* loop for counting number of digits */
+        while (temp != 0)
+        {
+            temp /= 10;
+            num_digits++;
+        }
+
         temp = num;
 
-        // Check Armstrong number
-        while (temp > 0)
+        while (temp != 0)
         {
-            currentdigit = temp % 10;
-            sum = sum + (currentdigit * currentdigit * currentdigit);
-            temp = temp / 10;
+            digit = temp % 10;
+            sum += round(pow(digit, num_digits));
+            temp /= 10;
         }
 
         if (sum == num)
         {
-          // Display the result 
-          cout << num << " ";
+            cout << num << " ";
         }
     }
 
+    cout << endl;
     return 0;
 }
